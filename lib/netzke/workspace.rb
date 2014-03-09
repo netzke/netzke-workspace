74af822594d60f4c48b048e2bd6e19beab289509
Max Gorin <gorinme@gmail.com>
2014-03-09 09:41:22 +0800

    $ bundle gem netzke-workspace

---
+++ lib/netzke/workspace.rb
@@ -0,0 +1,7 @@
+require "netzke/workspace/version"
+
+module Netzke
+  module Workspace
+    # Your code goes here...
+  end
+end
---
+++ lib/netzke/workspace/version.rb
@@ -0,0 +1,5 @@
+module Netzke
+  module Workspace
+    VERSION = "0.0.1"
+  end
+end
